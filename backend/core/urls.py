53e62c517aa768135431526e001eaff39f3b933b
Aziza <mosavimujtaba366@gmail.com>
2026-08-25 20:14:47 +1200

    Remove Django admin from URLs - not needed for Supabase API

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,12 +1,5 @@
-from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 
 urlpatterns = [
     path('api/', include('api.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += [
-        path('admin/', admin.site.urls),
-    ]
